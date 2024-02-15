a6c28ff6d33193045b71b8d9d958c57dbc243be3
лина <@DESKTOP-QOG2EQH>
2024-02-15 10:08:03 +0300

    Добавьте файлы проекта.

---
+++ pz na pare/FlightTicket.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_na_pare
+{
+    internal class FlightTicket
+    {
+        public enum TicketCategory
+        {
+            Economy,
+            Business
+        }
+
+        public static int EconomyTicketsSold { get; private set; }
+        public static int BusinessTicketsSold { get; private set; }
+
+        private string seatNumber;
+        private string passengerName;
+        private DateTime passengerBirthday;
+        private DateTime departureTime;
+
+        public TicketCategory Category { get; }
+        public string SeatNumber
+        {
+            get { return seatNumber; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Недопустимый номер места, выберите допустимое");
+                    Console.Write("Ваш номер: ");
+                    SeatNumber = Console.ReadLine();
+                }
+                else
+                {
+                    seatNumber = value;
+                }
+            }
+        }
+
+        public string PassengerName
+        {
+            get { return passengerName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Имя пассажира недопустимо. Введите правильно");
+                    Console.Write("Пассажирское имя ");
+                    PassengerName = Console.ReadLine();
+                }
+                else
+                {
+                    passengerName = value;
+                }
+            }
+        }
+
+        public DateTime PassengerBirthday
+        {
+            get { return passengerBirthday; }
+            set { passengerBirthday = value; }
+        }
+
+        public DateTime DepartureTime
+        {
+            get { return departureTime; }
+            set
+            {
+                if (value == DateTime.MinValue)
+                {
+                    Console.WriteLine("Время вылета не заполнено. Введите не пустое значение");
+                    Console.Write("Время вылета ");
+                    DepartureTime = DateTime.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    departureTime = value;
+                }
+            }
+        }
+
+        public decimal TicketPrice
+        {
+            get
+            {
+                if (Category == TicketCategory.Business)
+                {
+                    return (PassengerBirthday > DateTime.Today.AddYears(-10)) ? 10000 : 10000;
+                }
+                else if (Category == TicketCategory.Economy)
+                {
+                    return (PassengerBirthday > DateTime.Today.AddYears(-10)) ? 3500 : 7000;
+                }
+
+                return 0;
+            }
+        }
+
+        public FlightTicket(TicketCategory category, string seatNumber, string passengerName, DateTime passengerBirthday, DateTime departureTime)
+        {
+            if (category == TicketCategory.Economy && EconomyTicketsSold >= 30)
+            {
+                Console.WriteLine("Нельзя продать более тридцати билетов эконом-класса");
+                return;
+            }
+            else if (category == TicketCategory.Business && BusinessTicketsSold >= 6)
+            {
+                Console.WriteLine("Нельзя продать более шести билетов бизнесс-класса");
+                return;
+            }
+
+            Category = category;
+            SeatNumber = seatNumber;
+            PassengerName = passengerName;
+            PassengerBirthday = passengerBirthday;
+            DepartureTime = departureTime;
+
+            if (category == TicketCategory.Economy)
+            {
+                EconomyTicketsSold++;
+            }
+            else if (category == TicketCategory.Business)
+            {
+                BusinessTicketsSold++;
+            }
+        }
+
+        public void DisplayTicketInformation()
+        {
+            Console.WriteLine("Информация о билете:");
+            Console.WriteLine("Номер посадочного места: " + SeatNumber);
+            Console.WriteLine("Дата отправления и время вылета " + DepartureTime.ToString("dd/MM/yyyy HH:mm"));
+            Console.WriteLine("ФИО пассажира " + PassengerName);
+            Console.WriteLine("Категория билета: " + Category.ToString());
+        }
+
+        public void DisplayTimeRemaining()
+        {
+            TimeSpan timeRemaining = DepartureTime - DateTime.Now;
+            Console.WriteLine("Сколько времени осталось: " + timeRemaining.ToString(@"dd\.hh\:mm"));
+        }
+
+        public static void DisplaySoldTicketsCount()
+        {
+            Console.WriteLine("Билетов эконом-класса распродано: " + EconomyTicketsSold);
+            Console.WriteLine("Билетов бизнес-класса распродано: " + BusinessTicketsSold);
+        }
+    }
+}
+
---
+++ pz na pare/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_na_pare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FlightTicket ticket1 = new FlightTicket(FlightTicket.TicketCategory.Economy, "3", "Астафьева Анна", new DateTime(2006, 06, 10), new DateTime(2024, 07, 13, 12, 0, 0));
+            ticket1.DisplayTicketInformation();
+            ticket1.DisplayTimeRemaining();
+            Console.WriteLine();
+
+            FlightTicket ticket2 = new FlightTicket(FlightTicket.TicketCategory.Business, "17", "Мухамбетова Алина", new DateTime(2006, 12, 26), new DateTime(2024, 07, 13, 12, 30, 0));
+            ticket2.DisplayTicketInformation();
+            ticket2.DisplayTimeRemaining();
+            Console.WriteLine();
+
+            FlightTicket ticket3 = new FlightTicket(FlightTicket.TicketCategory.Business, "18", "Сарбаева Азалия", new DateTime(2006, 07, 30), new DateTime(2024, 07, 13, 12, 30, 0));
+            ticket3.DisplayTicketInformation();
+            ticket3.DisplayTimeRemaining();
+            Console.WriteLine();
+
+            FlightTicket ticket4 = new FlightTicket(FlightTicket.TicketCategory.Business, "19", "Винс Елена", new DateTime(2006, 07, 10), new DateTime(2024, 07, 13, 12, 30, 0));
+            ticket4.DisplayTicketInformation();
+            ticket4.DisplayTimeRemaining();
+            Console.WriteLine();
+
+            FlightTicket.DisplaySoldTicketsCount();
+        }
+    }
+}
